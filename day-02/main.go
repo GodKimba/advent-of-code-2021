f9a6616a216815835ea078d5765b0d5546c54efd
GodKimba <rafaelfadel2@gmail.com>
2022-05-25 00:49:19 -0300

    started the day-02, function working, but getting wrong output

--- day-02/main.go
+++ day-02/main.go
@@ -15,18 +15,32 @@ func main() {
 	defer f.Close()
 
 	sum := 0
+	count := 0
+	finalSlice := make([]int, 0)
 	slc := make([]int, 0)
 	s := bufio.NewScanner(f)
 	for s.Scan() {
 		var n int
-		_, err := fmt.Sscan(s.Text(), "%d", &n)
+		_, err := fmt.Sscanf(s.Text(), "%d", &n)
 		if err != nil {
-			log.Fatalf("could not read %s: %d", s.Text(), err)
+			log.Fatalf("could not read %s: %v", s.Text(), err)
 		}
 		slc = append(slc, n)
+	}
+	for i := 0; i < len(slc)-3; i++ {
+		sum = slc[i] + slc[i+2] + slc[i+3]
+
+		finalSlice = append(finalSlice, sum)
+	}
+	
+	for i := 0; i < len(finalSlice)-1; i++ {
+		
+		if finalSlice[i] < finalSlice[i+1] {
+			count += 1
+		}
 	}
 	if err := s.Err(); err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(sum)
+	fmt.Println(count)
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/GodKimba/advent-of-code-2021
+
+go 1.18
